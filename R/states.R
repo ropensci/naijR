387e219179841f3123e5217101f6819cfb6ef60b
BroVic <victorordu@outlook.com>
2016-05-02 17:26:41 +0100

    Initial commit

---
+++ R/states.R
@@ -0,0 +1,25 @@
+# Draft Function
+# A list of the 36 States of the Federation and FCT
+states <- function()
+  {
+  states <- c("Abia", "Adamawa",
+              "Akwa Ibom", "Anambra",
+              "Bauchi", "Bayelsa",
+              "Benue", "Borno",
+              "Cross River", "Delta",
+              "Ebonyi", "Edo",
+              "Ekiti", "Enugu",
+              "Federal Capital Territory",
+              "Gombe", "Imo",
+              "Jigawa", "Kaduna",
+              "Kano", "Katsina",
+              "Kebbi", "Kogi",
+              "Kwara", "Lagos",
+              "Nasarawa", "Niger",
+              "Ogun", "Ondo", "Osun",
+              "Oyo", "Plateau",
+              "Rivers", "Sokoto",
+              "Taraba", "Yobe",
+              "Zamfara")
+  print(states)
+}
