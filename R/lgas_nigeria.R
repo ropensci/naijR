d6f1ca1e8d4e6bfb592246264e3bc3171039ddfd
BroVic <victorordu@outlook.com>
2016-06-07 20:37:30 +0100

    Initial commit.

---
+++ R/lgas_nigeria.R
@@ -0,0 +1,10 @@
+#' Local Government Areas of Nigeria
+#'
+#' A dataset containing the 774 Local Government Areas of Nigeria
+#' 
+#' @format A dataframe with 774 rows and 2 columns
+#' \describe{
+#'    \item{lga}{Local Government Area}
+#'    \item{state}{State of the Federation}
+#'    }
+"lgas_nigeria"
