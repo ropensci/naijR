b9b43d2e0ae21979c2232a4c671740f08b2ecc16
BroVic <victorordu@outlook.com>
2016-10-09 18:20:52 +0100

    Simplify definition further
    Removed extraneous conditional statement.

--- R/states.R
+++ R/states.R
@@ -1,8 +1,8 @@
 #' Display States of the Federal Republic of Nigeria
 #' 
 #' 
-#' @param gpz Geopolitical zone. Default is \code{NULL}; optionally \code{"nc", 
-#' "ne", "nw", "se", "ss"} and \code{"sw"} (see \code{Details}).
+#' @param gpz Geopolitical zone. Default is \code{NULL}; optionally \code{"nc",
+#'  "ne", "nw", "se", "ss"} and \code{"sw"} (see \code{Details}).
 #' @return The States of Nigeria as a whole or by zones
 #' @details gpz A geo-political zone, in the Nigerian 
 #' context, is a national subdivision that groups contiguous states. 
@@ -15,6 +15,10 @@
 #' states("se")
 states <- function(gpz = c("all", "nc", "ne", "nw", "se", "ss", "sw"))
   {
+  # print out all states
+  # print out states by gpz
+  # print out all states but sort them along the lines of gpz
+  
   s <- c("Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa",
          "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti",
          "Enugu", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina",
@@ -22,19 +26,11 @@ states <- function(gpz = c("all", "nc", "ne", "nw", "se", "ss", "sw"))
          "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba",
          "Yobe", "Zamfara")
   gpz <- match.arg(gpz)
-  if (is.character(gpz))
-  {
-    switch (gpz,
-            all = s,
-            nc = s <- s[c(7, 22, 23, 25, 26, 31)],
-            ne = s <- s[c(2, 5, 8, 15, 34, 35)],
-            nw = s <- s[c(17:21, 33, 36)],
+  if (is.character(gpz)) {
+    switch (gpz, all = s, nc = s <- s[c(7, 22:23, 25:26, 31)],
+            ne = s <- s[c(2, 5, 8, 15, 34:35)], nw = s <- s[c(17:21, 33, 36)],
             se = s <- s[c(1, 4, 11, 14, 16)],
-            ss = s <- s[c(3, 6, 9, 10, 12, 32)],
-            sw = s <- s[c(13, 24, 27:30)]
-    )
+            ss = s <- s[c(3, 6, 9, 10, 12, 32)], sw = s <- s[c(13, 24, 27:30)])
     s
   }
-  else
-    stop("supply of non-character value(s) to argument 'gpz'")
 }
