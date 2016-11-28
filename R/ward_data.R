992acc2427a927763896acab3f9b87718d0116c9
BroVic <victorordu@outlook.com>
2016-11-28 01:31:14 +0100

    Initial commit

---
+++ R/ward_data.R
@@ -0,0 +1,11 @@
+#' Nigeria political wards by Local Government Area
+#'
+#' A complete list of political wards in theLocal Government Areas of Nigeria.
+#' 
+#' @format A data frame with 8799 rows and 2 columns:
+#' \describe{
+#'    \item{LGA}{Name of Local Government Area}
+#'    \item{Ward}{Name of political ward}
+#' }
+#' @source \url{http://www.inecnigeria.org/wp-content/uploads/2013/03/RA-LGA-ANALYSIS-NATIONWIDE.pdf}
+"ward_data" 
