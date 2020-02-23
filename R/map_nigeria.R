e22da2ef4bbcf87a389384e75d7e7c6ad7f22c29
Victor Ordu <victorordu@outlook.com>
2020-02-23 14:14:40 +0100

    Make more robust
    The mapping function now returns the object of class 'map', as well as transmits additional arguments via '...' to the internal mapping function as well.

--- R/map_nigeria.R
+++ R/map_nigeria.R
@@ -1,50 +1,57 @@
-# Copyright (C) 2019 Victor Ordu.
+# Source file: map_nigeria.R
+#
+# GPL-3 License
 # 
+# Copyright (C) 2019-2020 Victor Ordu.
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 #' Map of Nigeria
-#' 
+#'
 #' A basic map of the Federal Republic of Nigeria.
-#' 
+#'
 #' @details This function is essentially a wrapper to \code{maps::map}.
-#' 
+#'
 #' @return \code{NULL} invisibly; results in the drawing of a map of Nigeria.
 #'
-#' @param show.neighbours logical; \code{TRUE} to display borders of 
-#' neighbouring countries. 
+#' @param show.neighbours logical; \code{TRUE} to display borders of
+#' neighbouring countries.
 #' @param ... Further arguments for function \code{\link[maps]{map}}
-#' 
+#'
 #' @import mapdata
 #' @importFrom maps map
-#' 
-#' @examples 
+#'
+#' @examples
 #' map_ng() # Draw a map with default settings
 #' map_ng(TRUE) # Display portions of neighbouring countries' borders
-#' 
+#'
 #' @export
 map_ng <- function(show.neighbours = FALSE, ...)
 {
-    db <- 'worldHires'
-    maps::map(db, "Nigeria", ...)
-    if (show.neighbours) jn(db)
+  stopifnot(is.logical(show.neighbours))
+  db <- 'mapdata::worldHires'
+  mp <- maps::map(db, "Nigeria", ...)
+  if (show.neighbours) 
+    mp <- jn(db, ...)
+  invisible(mp)
 }
 
 
 
 
 #' @importFrom maps map
-jn <- function(data, ...) 
+jn <- function(mapObj, ...)
 {
-    maps::map(data, c("Cameroon", "Chad", "Niger", "Benin"), add = TRUE, ...)
+  maps::map(mapObj, c("Cameroon", "Chad", "Niger", "Benin"), add = TRUE, ...)
 }
