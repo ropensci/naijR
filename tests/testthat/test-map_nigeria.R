13b5b950dc902f22142d7d288cea8be9418571aa
Victor Ordu <victorordu@outlook.com>
2020-04-14 18:26:22 +0100

    Update unit tests
    Added new tests for the mapping functionality and fixed those that were broken with the new approach that was introduced i.e. use of shapefile as data source and manipulation of 'sp' objects.

--- tests/testthat/test-map_nigeria.R
+++ tests/testthat/test-map_nigeria.R
@@ -16,37 +16,43 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
+library(testthat)
+library(naijR)
 
 test_that("Input is validated", {
   myerr <- "is.logical(show.neighbours) is not TRUE"
   
   expect_error(map_ng(999), myerr, fixed = TRUE)
   expect_error(map_ng(NULL), myerr, fixed = TRUE)
-  expect_error(map_ng(NA), "!is.na\\(show.neighbours\\) is not TRUE")
+  expect_error(map_ng(NA), "missing value where TRUE/FALSE needed")
   expect_error(map_ng('TRUE'), myerr, fixed = TRUE)
   expect_error(map_ng(pi), myerr, fixed = TRUE)
+  expect_warning(map_ng(TRUE, plot = FALSE), 
+                 "Display of neighbouring countries is disabled")
 })
 
 
 
 test_that("'map' object is properly created", {
   mp1 <- map_ng(plot = FALSE)
-  mp2 <- map_ng(show = TRUE, plot = FALSE)
+  mp2 <- suppressWarnings(map_ng(show = TRUE, plot = FALSE))
   
   expect_is(mp1, 'map')
   expect_type(mp1, 'list')
+  expect_s3_class(mp1, 'map')
   expect_length(mp1, 4L)
   expect_identical(names(mp1), c("x", "y", 'range', 'names'))
-  expect_identical(mp1$names, 'Nigeria')
-  expect_identical(mp1$x, readRDS('data/map_xcoord.rds'))
-  expect_identical(mp1$y, readRDS('data/map_ycoord.rds'))
-  expect_equal(signif(mp1$range, 7), c(2.681958, 14.67419, 4.272489, 13.89193))
-  expect_equal(signif(mp2$range, 7), c(0.1666734, 24.00166, 1.654186, 23.52112))
-  expect_identical(mp2$names, c("Chad", "Niger", "Benin", "Nigeria","Cameroon"))
-  expect_false(identical(mp1$x, mp2$x))
-  expect_false(identical(mp1$y, mp2$y))
-  expect_true(length(mp1$x) < length(mp2$x))
-  expect_true(length(mp1$y) < length(mp2$y))
+  expect_length(mp1$names, 41)
+  expect_true(identical(mp1$x, mp2$x))
+  expect_true(identical(mp1$y, mp2$y))
+  expect_false(length(mp1$x) < length(mp2$x))
+  expect_false(length(mp1$y) < length(mp2$y))
   expect_true(all(mp1$x %in% mp2$x))
   expect_true(all(mp1$y %in% mp2$y))
+  
+  rng <- c(2.668534, 14.678820, 4.273007, 13.894420)
+  expect_equal(signif(mp1$range, 7), rng)
+  expect_equal(signif(mp2$range, 7), rng)
+  
+  for (i in states()) expect_match(mp2$names, i, all = FALSE)
 })
