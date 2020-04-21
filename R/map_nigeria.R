f8f1614603dfe812bb5557f5d9d2749c7567f178
Victor Ordu <victorordu@outlook.com>
2020-04-21 14:07:15 +0100

    Add parameter to allow for selective plotting of regions

--- R/map_nigeria.R
+++ R/map_nigeria.R
@@ -19,14 +19,16 @@
 
 #' Map of Nigeria
 #'
-#' A basic map of the Federal Republic of Nigeria.
+#' A map of the Federal Republic of Nigeria.
 #' 
 #' @details This function is essentially a wrapper to \code{maps::map}.
 #'
-#' @return An object of class \code{map}, invisibly; as a side-effect,
-#' results in the drawing of a map of Nigeria.
+#' @importFrom graphics par
+#' @importFrom maps map
 #' 
-#' @param style The type of map to be drawn. Current options are \code{basic}
+#' @param state A character vector of a list of Nigerian States to be displayed.
+#' The default value is to print all States.
+#' @param style The type of map to be drawn. Current options are \code{plain}
 #' and \code{choropleth}.
 #' @param var A variable to be plotted in a map (applicable only to 
 #' \code{choropleth})
@@ -36,21 +38,36 @@
 #' neighbouring countries.
 #' @param ... Further arguments for function \code{\link[maps]{map}}
 #' 
-#' @importFrom graphics par
-#' @importFrom maps map
+#' @return An object of class \code{map}, invisibly; as a side-effect,
+#' results in the drawing of a map of Nigeria.
 #'
 #' @examples
 #' map_ng() # Draw a map with default settings
+#' 
+#' ## Choropleth
+#' set.seed(4)
+#' vals <- sample(0:6, 37, TRUE)
+#' brk <- seq(0, 6, 2)
+#' map_ng('choropleth', vals, brk)
 #'
 #' @return An object of class \code{maps} containing the data used to draw the
-#' map and which can be used for additional calls to \code{\link[maps]{map}}.
+#' map and which can be used for additional calls to \code{\link[maps]{map}} or
+#' other similar functions (e.g. \code{\link[graphics]{plot}}).
 #'
 #' @export
-map_ng <- function(style = c("basic", 'choropleth'),
+map_ng <- function(state = character(),
+                   style = c("plain", 'choropleth'),
                    var = NULL,
-                   breaks = NULL, 
-                   show.neighbours = FALSE, ...)
+                   breaks = NULL,
+                   show.neighbours = FALSE,
+                   ...)
 {
+  # TODO: Add an argument for labelling the States
+  all.st <- states(with.fct = TRUE)
+  if (length(state) == 0L && !is.null(state))
+    state <- all.st
+  if (!any(state %in% all.st))
+    stop("One or more elements of 'state' is not a Nigerian state")
   if (is.null(style))
     stop("Invalid input for 'style'.")
   style <- match.arg(style)
@@ -58,23 +75,38 @@ map_ng <- function(style = c("basic", 'choropleth'),
   if (show.neighbours)
     warning("Display of neighbouring countries is disabled")
   dt <- .getMapData()
+  
   dots <- list(...)
   plot <- TRUE
   if ('plot' %in% names(dots))
     plot <- dots[['plot']]
   fill <- FALSE
-  col <- 1L 
+  col <- 1L
   if (style == 'choropleth') {
+    if (is.null(var) || is.null(breaks))
+      stop("'var' and 'breaks' must be supplied to plot chropleth maps")
     fill <- TRUE
     colobj <- .prepareChoroplethColors(var, breaks, ...)
     col <- colobj$colors
   }
-  else if (style == 'basic' && 'fill' %in% names(dots)) {
+  else if (style == 'plain' && 'fill' %in% names(dots)) {
     fill <- dots[['fill']]
   }
-  m <- map(dt, plot = plot, fill = fill, col = col)
-  # if (plot && style == 'choropleth')
-  #   legend("bottomleft", legend = colobj$bins, fill = colobj$scheme)
+  m <- map(
+    dt,
+    region = state,
+    plot = plot,
+    fill = fill,
+    col = col
+  )
+  if (plot && style == 'choropleth')
+    legend(
+      x = 12,
+      y = 5,
+      legend = colobj$bins,
+      fill = colobj$scheme,
+      xpd = NA
+    )
   invisible(m)
 }
 
@@ -115,12 +147,13 @@ map_ng <- function(style = c("basic", 'choropleth'),
 .prepareChoroplethColors <- 
   function(x, brk, col = c("grey", "red", "green", "blue"))
   {
-    # TODO: Validate `x` and `brk`
     stopifnot(is.atomic(x), is.atomic(brk))
+    if (is.array(x))
+      stop("Expected dim(x) to evaluate to NULL")
     # TODO: Set limits for variables and brk
     col <- match.arg(col)  # TODO: Accept numeric input
     pal <- col %>% toTitleCase %>% paste0("s")
-    bins <- length(brk - 1)
+    bins <- length(brk) - 1
     cr <- RColorBrewer::brewer.pal(bins, pal)
     ind <- findInterval(x, brk, all.inside = TRUE)
     list(colors = cr[ind], scheme = cr, bins = levels(cut(x, brk)))
