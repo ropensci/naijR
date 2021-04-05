2db080e1f3110573ed63d7f58c820264f88c6562
Victor Ordu <victorordu@outlook.com>
2021-04-05 19:23:01 +0100

    Update copyright notice

--- R/lgas_nigeria.R
+++ R/lgas_nigeria.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Victor Ordu.
+# Copyright (C) 2019-2021 Victor Ordu.
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- R/map_nigeria.R
+++ R/map_nigeria.R
@@ -2,7 +2,7 @@
 #
 # GPL-3 License
 #
-# Copyright (C) 2019-2020 Victor Ordu.
+# Copyright (C) 2019-2021 Victor Ordu.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
