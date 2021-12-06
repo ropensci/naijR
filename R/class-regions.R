d987c4d22da7416fc1f91dcbaea15bccc3d7146f
Victor Ordu <victorordu@outlook.com>
2021-12-06 12:01:04 +0100

    Bump version to 3.4 (Patch)

--- R/class-regions.R
+++ R/class-regions.R
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-globalVariables(c("lgas_nigeria", "state", "lga"))
+globalVariables(c("lgas_nigeria", "state", "lga", "gpz"))
 
 #' States of the Federal Republic of Nigeria
 #' 
