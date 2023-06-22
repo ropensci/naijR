9cece757d7ef8691ac8a2c5466f5d28234dc60f8
Victor Ordu <victorordu@outlook.com>
2023-06-22 12:42:05 +0100

    Reorganize administrative data
    A new dataset for States has been included and the code for generating it as well as that for LGAs has been totally reorganized. Attempts have been made to harmonize this for both datasets to make future modifications easier, including the possible inclusion of ward data.

---
+++ R/regiondata.R
@@ -0,0 +1,23 @@
+#' Local Government Areas of Nigeria
+#'
+#' A dataset containing the 774 Local Government Areas of Nigeria
+#'
+#' @format A dataframe with 774 rows and 2 columns
+#' \describe{
+#'    \item{lga}{Local Government Area}
+#'    \item{state}{State of the Federation}
+#'    }
+"lgas_nigeria"
+
+
+
+#' States of Nigeria
+#' 
+#' A dataset of the 36 States and Federal Capital Territory of Nigeia
+#' 
+#' @format A \code{data.frame} with 37 rows and 2 columns
+#' \describe{
+#'   \item{Code}{ISO 3661 Alpha 2 code}
+#'   \item{Name}{Name of the State or Territory}
+#' }
+"states_nigeria"
---
+++ data-raw/admin.R
@@ -0,0 +1,38 @@
+# admin.R
+# Create package data for administrative regions from raw data
+#
+# Dependencies ----
+suppressPackageStartupMessages(library(here))
+library(usethis)
+devtools::load_all()
+
+check_exist <- function(obj) {
+    fname <- paste0(deparse(substitute(obj)), ".rda")
+    file.exists(here("data", fname))
+}
+
+# States ----
+# We'll include ISO 3166-2 codes for the States
+# 
+states_nigeria <- ISOcodes::ISO_3166_2 |>
+  subset(startsWith(Code, "NG"), select = Code:Name) |>
+  transform(Name = sub("^Abuja\\s", "", Name)) |>
+  setNames(c("isocode", "state")) |>
+  .__addGPZ("state", "gpz")
+
+use_data(states_nigeria, overwrite = check_exist(states_nigeria))
+
+
+# Local Government Areas ----
+file <- here("data-raw", "list_of_local_government_areas_of_nigeria-1729j.csv")
+
+lgas_nigeria <- read.csv(file) |>
+  subset(select = -SNo) |>
+  transform(
+    State = sub("(\\s)State", "", State) |>
+            sub("Nassarawa", "Nasarawa", x = _) |>
+            sub("FCT", "Federal Capital Territory", x = _)
+  ) |>
+  setNames(c("lga", "state"))
+
+use_data(lgas_nigeria, overwrite =  check_exist(lgas_nigeria))
