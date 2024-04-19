67c06d910a69aa30983fd13f14711b5b8b16f11a
Victor Ordu <victorordu@outlook.com>
2024-04-19 17:38:40 +0100

    Fix some typos

--- R/regdata.R
+++ R/regdata.R
@@ -2,13 +2,13 @@
 #
 # GPL-3 License
 #
-# Copyright (C) 2019-2022 Victor Ordu.
+# Copyright (C) 2019-2024 Victor Ordu.
 
 #' Local Government Areas of Nigeria
 #'
-#' A dataset containing the 774 Local Government Areas of Nigeria
+#' A dataset containing the 774 Local Government Areas of Nigeria.
 #'
-#' @format A dataframe with 774 rows and 2 columns
+#' @format A data frame with 774 rows and 2 columns
 #' \describe{
 #'    \item{lga}{Local Government Area}
 #'    \item{state}{State of the Federation}
@@ -18,9 +18,10 @@
 
 
 
+
 #' States of Nigeria
 #' 
-#' A dataset of the 36 States and Federal Capital Territory of Nigeia
+#' A dataset of the 36 States and Federal Capital Territory of Nigeria.
 #' 
 #' @format A \code{data.frame} with 37 rows and 2 columns
 #' \describe{
@@ -29,3 +30,14 @@
 #'   \item{gpz}{Geo-political zone}
 #' }
 "states_nigeria"
+
+
+
+
+#' Distance By Road of Nigerian State Capitals
+#' 
+#' A distance matrix with the road distance in kilometers for Nigeria's
+#' State capitals.
+#'
+#' @format An object of class \code{dist}.
+"ngdist"
